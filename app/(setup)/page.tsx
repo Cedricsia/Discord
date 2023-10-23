6e4ab38c9cff9b14e5d35163af6554f748ce7a89
Cedricsia <cedricsia@hotmail.fr>
2023-10-23 14:49:47 +0200

    add form

--- app/(setup)/page.tsx
+++ app/(setup)/page.tsx
@@ -1,24 +1,23 @@
-import {redirect } from "next/navigation"
+import { redirect } from "next/navigation";
 import { initialProfile } from "@/lib/initial-profile";
 import { db } from "@/lib/db";
+import { InitialModal } from "@/components/modals/initial-modal";
 const SetupPage = async () => {
-    const profile = await initialProfile();
-    const server = await db.server.findFirst({
+  const profile = await initialProfile();
+  const server = await db.server.findFirst({
+    where: {
+      members: {
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+  });
+  if (server) {
+    return redirect(`/server/${server.id}`);
+  }
 
-        where: {
-            members:{
-                some:{
-                    profileId: profile.id
-                }
-            }
-        }
-    });
-    if(server){
-       return redirect(`/server/${server.id}`) 
-    }
-
-
-  return <div>Create a Server</div>;
+  return <InitialModal />;
 };
 
 export default SetupPage;
