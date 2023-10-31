5b530d9643aa4068f87f2ec764b10063a942bff2
Cedricsia <cedricsia@hotmail.fr>
2023-10-31 10:27:49 +0100

    sockeckt instance

--- types.ts
+++ types.ts
@@ -1,5 +1,16 @@
+import { Server as Netserver, Socket } from "net";
+import { NextApiResponse } from "next";
+import { Server as SocketIOServer } from "socket.io";
 import { Profile, Member, Server } from "@prisma/client";
 
 export type ServerWithMembersWithProfiles = Server & {
   members: (Member & { profile: Profile })[];
+};
+
+export type NextApiResponseServerIO = NextApiResponse & {
+  socket: Socket & {
+    server: Netserver & {
+      io: SocketIOServer;
+    };
+  };
 };
